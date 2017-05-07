cf95deab698eaccecbe2d3eb7dfd84555586768e
wodrow <wodrow451611cv@gmail.com>
2017-05-07 09:44:49 +0800

    update models

---
+++ common/models/UserInfo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wodrow
+ * Date: 17-5-7
+ * Time: 上午9:43
+ */
+
+namespace common\models;
+
+
+class UserInfo extends \common\models\tables\UserInfo
+{
+
+}
--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -1,8 +1,10 @@
 <?php
 namespace frontend\models;
 
+use common\models\tables\UserInfo;
 use yii\base\Model;
 use common\models\User;
+use yii\db\Exception;
 
 /**
  * Signup form
@@ -46,13 +48,24 @@ class SignupForm extends Model
         if (!$this->validate()) {
             return null;
         }
-        
-        $user = new User();
-        $user->username = $this->username;
-        $user->email = $this->email;
-        $user->setPassword($this->password);
-        $user->generateAuthKey();
-        
-        return $user->save() ? $user : null;
+        $trans = \Yii::$app->db->beginTransaction();
+        try{
+            $user = new User();
+            $user->username = $this->username;
+            $user->email = $this->email;
+            $user->setPassword($this->password);
+            $user->generateAuthKey();
+
+            if ($user->save()){
+                $user_info = new UserInfo();
+                $user_info->user_id = $user->id;
+                $user_info->save();
+            }
+            $trans->commit();
+            return $user;
+        }catch (Exception $e){
+            $trans->rollBack();
+            return null;
+        }
     }
 }
